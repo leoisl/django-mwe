ece6af88ad90a2d90313c656fcf5ee29c740d5f6
Gibraan Rahman <gibraan.rahman@czbiohub.org>
2024-04-30 13:43:24 -0700

    Add Add/Edit message API endpoints

--- hello/echo/urls.py
+++ hello/echo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('echo_static/view/<int:pk>/', views.MessageView.as_view(), name='view_message'),
     path('api/get_message/<int:id>/', api.get_message, name="get_message"),
     path('api/get_all_messages/', api.get_all_messages, name="get_all_messages"),
+    path('api/add_message/', api.AddMessage.as_view(), name="add_message"),
+    path('api/edit_message/', api.EditMessage.as_view(), name="edit_message")
 ]
