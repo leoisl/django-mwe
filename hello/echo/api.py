63e8b796f711a57fc4de40a4052c36d3a91ae396
Gibraan Rahman <gibraan.rahman@czbiohub.org>
2024-04-30 13:23:12 -0700

    Add full table API endpoint

--- hello/echo/api.py
+++ hello/echo/api.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.core import serializers
 
 from .models import Message
 
@@ -7,3 +8,7 @@ def get_message(request, id):
     message = Message.objects.get(pk=id)
     return JsonResponse({'message': message.message})
 
+
+def get_all_messages(request):
+    all_messages = list(Message.objects.all().values('id', 'message'))
+    return JsonResponse(all_messages, safe=False)
